bf102574b5e51cd54ee0ea10aaa84d1b43b6285f
Teddee Jordan <teddeejordan@Teddees-Air.attlocal.net>
2025-01-22 13:37:05 -0600

    removed change to metro config

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,4 @@
 const { getDefaultConfig: getDefaultExpoConfig } = require("@expo/metro-config")
-const path = require("path")
-
-const ALIASES = {
-      "*": "./src/*",
-      "@assets/*": "./assets",
-      "@components/*": "./src/components",
-      "@screens/*": "./src/screens",
-      "@navigation/*": "./src/navigation",
-      "@constants/*": "./src/constants",
-}
-
 metroConfig = (() => {
   const config = getDefaultExpoConfig(__dirname)
 
@@ -25,29 +14,7 @@ metroConfig = (() => {
     sourceExts: [...resolver.sourceExts, "svg"],
   }
 
-  config.resolver.resolveRequest = (context, moduleName, platform) => {
-    return context.resolveRequest(
-      context,
-      ALIASES[moduleName] ?? moduleName,
-      platform
-    )
-  }
- return config
+  return config
 })()
 
 module.exports = metroConfig
-
-// module.exports = (async () => {
-//   const {
-//     resolver: { sourceExts, assetExts },
-//   } = await getDefaultConfig();
-//   return {
-//     transformer: {
-//       babelTransformerPath: require.resolve('react-native-svg-transformer'),
-//     },
-//     resolver: {
-//       assetExts: assetExts.filter(ext => ext !== 'svg'),
-//       sourceExts: [...sourceExts, 'svg'],
-//     },
-//   };
-// })();
